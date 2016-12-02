6f581d0c66d68f5a0d3f48450e7d77c0e87aacb3
Ramachandran G <g.rmchndrn@gmail.com>
2016-12-03 01:56:08 +0530

    Basic Typescript app is up and running.

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+class App {
+    public message: string;
+}
+
+let app = new App();
+app.message = "<h1>Hello world!!</h1>";
+
+document.write(app.message);
+document.write(app.message);
+document.write(app.message);
